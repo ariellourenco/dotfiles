59410452ea952b5759169a22c805eb76e4960be5
Ariel Lourenco <ariellourenco@users.noreply.github.com>
2025-03-04 10:07:34 -0300

    Add Windows PowerShell config to machine setup
    Introduces Windows PowerShell setup to WinGet configuration file,
    copying the available PowerShell profile to the user's home directory
    and installing any required PowerShell modules for profile functionality.
    
    This change introduces the following changes:
    
    - Updates the configuration.dsc.yaml file.
    - Moves the PowerShell profile from terminal folder to windows folder.
    - Updates the folder documentation with instruction and requirements on
      how to have the Windows PowerShell correctly configured as part of the
      machine setup.

---
+++ windows/Profile.ps1
@@ -0,0 +1,288 @@
+﻿using namespace System.Management.Automation
+using namespace System.Management.Automation.Language
+
+# Import necessary modules
+Import-Module -Name posh-git
+Import-Module -Name Terminal-Icons
+
+# Configure the PSReadLine module
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -MaximumHistoryCount 100
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+# Configure key handlers for history search
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Key handler for capturing the screen
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d,Ctrl+c' -Function CaptureScreen
+
+# Key handler for showing command history in Out-GridView
+Set-PSReadLineKeyHandler -Key F7 `
+    -BriefDescription History `
+    -LongDescription 'Show command history' `
+    -ScriptBlock {
+        $pattern = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
+
+        if ($pattern) {
+            $pattern = [regex]::Escape($pattern)
+        }
+
+        $history = [System.Collections.ArrayList]@(
+            $last = ''
+            $lines = ''
+            foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath)) {
+                if ($line.EndsWith('`')) {
+                    $line = $line.Substring(0, $line.Length - 1)
+                    $lines = if ($lines) {
+                        "$lines`n$line"
+                    } else {
+                        $line
+                    }
+                    continue
+                }
+
+                if ($lines) {
+                    $line = "$lines`n$line"
+                    $lines = ''
+                }
+
+                if (($line -cne $last) -and (!$pattern -or ($line -match $pattern))) {
+                    $last = $line
+                    $line
+                }
+            }
+        )
+
+        $history.Reverse()
+
+        $command = $history | Out-GridView -Title History -PassThru
+
+        if ($command) {
+            [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+            [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
+        }
+    }
+
+# Key handler for saving a command in history without executing
+Set-PSReadLineKeyHandler -Key Alt-w `
+    -BriefDescription SaveInHistory `
+    -LongDescription "Save current line in history but do not execute" `
+    -ScriptBlock {
+        param($key, $arg)
+
+        $line = $null
+        $cursor = $null
+
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        [Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($line)
+        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    }
+
+#region Smart Insert/Delete
+
+# Key handler for smart insertion of quotes
+Set-PSReadLineKeyHandler -Key '"',"'" `
+    -BriefDescription SmartInsertQuote `
+    -LongDescription "Insert paired quotes if not already on a quote" `
+    -ScriptBlock {
+        param($key, $arg)
+
+        $quote = $key.KeyChar
+
+        $selectionStart = $null
+        $selectionLength = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
+
+        $line = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+        # If text is selected, just quote it without any smarts
+        if ($selectionStart -ne -1) {
+            [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+                $selectionStart,
+                $selectionLength,
+                $quote + $line.SubString($selectionStart, $selectionLength) + $quote
+            )
+            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
+            return
+        }
+
+        $ast = $null
+        $tokens = $null
+        $parseErrors = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$parseErrors, [ref]$null)
+
+        function FindToken {
+            param($tokens, $cursor)
+
+            foreach ($token in $tokens) {
+                if ($cursor -lt $token.Extent.StartOffset) { continue }
+                if ($cursor -lt $token.Extent.EndOffset) {
+                    $result = $token
+                    $token = $token -as [StringExpandableToken]
+                    if ($token) {
+                        $nested = FindToken $token.NestedTokens $cursor
+                        if ($nested) { $result = $nested }
+                    }
+
+                    return $result
+                }
+            }
+            return $null
+        }
+
+        $token = FindToken $tokens $cursor
+
+        # If we're on or inside a quoted string token, handle accordingly
+        if ($token -is [StringToken] -and $token.Kind -ne [TokenKind]::Generic) {
+            if ($token.Extent.StartOffset -eq $cursor) {
+                [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote ")
+                [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+                return
+            }
+
+            if ($token.Extent.EndOffset -eq ($cursor + 1) -and $line[$cursor] -eq $quote) {
+                [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+                return
+            }
+        }
+
+        if ($null -eq $token -or
+            $token.Kind -eq [TokenKind]::RParen -or $token.Kind -eq [TokenKind]::RCurly -or $token.Kind -eq [TokenKind]::RBracket) {
+            if ($line[0..$cursor].Where{$_ -eq $quote}.Count % 2 -eq 1) {
+                [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
+            } else {
+                [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote")
+                [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+            }
+            return
+        }
+
+        if ($token.Extent.StartOffset -eq $cursor) {
+            if ($token.Kind -eq [TokenKind]::Generic -or $token.Kind -eq [TokenKind]::Identifier -or
+                $token.Kind -eq [TokenKind]::Variable -or $token.TokenFlags.hasFlag([TokenFlags]::Keyword)) {
+                $end = $token.Extent.EndOffset
+                $len = $end - $cursor
+                [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+                    $cursor,
+                    $len,
+                    $quote + $line.SubString($cursor, $len) + $quote
+                )
+                [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($end + 2)
+                return
+            }
+        }
+
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
+    }
+
+#endregion Smart Insert/Delete
+
+# Additional handlers and configurations omitted for brevity...
+# Sometimes you want to get a property or invoke a member on what you've entered so far,
+# but you need parentheses to do that. This binding will help by putting parentheses
+# around the current selection, or if nothing is selected, the whole line.
+Set-PSReadLineKeyHandler -Key 'Alt+(' `
+    -BriefDescription ParenthesizeSelection `
+    -LongDescription "Put parentheses around the selection or entire line and move the cursor to after the closing parenthesis" `
+    -ScriptBlock {
+        param($key, $arg)
+
+        $selectionStart = $null
+        $selectionLength = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
+
+        $line = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+        if ($selectionStart -ne -1) {
+            [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+                $selectionStart,
+                $selectionLength,
+                '(' + $line.Substring($selectionStart, $selectionLength) + ')'
+            )
+            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
+        } else {
+            [Microsoft.PowerShell.PSConsoleReadLine]::Replace(0, $line.Length, '(' + $line + ')')
+            [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+        }
+    }
+
+# Each time you press Alt+', this key handler will change the token under or before the cursor.
+# It will cycle through single quotes, double quotes, or no quotes each time it is invoked.
+Set-PSReadLineKeyHandler -Key "Alt+'" `
+    -BriefDescription ToggleQuoteArgument `
+    -LongDescription "Toggle quotes on the argument under the cursor" `
+    -ScriptBlock {
+        param($key, $arg)
+
+        $ast = $null
+        $tokens = $null
+        $errors = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+
+        $tokenToChange = $null
+        foreach ($token in $tokens) {
+            $extent = $token.Extent
+            if ($extent.StartOffset -le $cursor -and $extent.EndOffset -ge $cursor) {
+                $tokenToChange = $token
+
+                # If the cursor is at the end (it's really 1 past the end) of the previous token,
+                # we only want to change the previous token if there is no token under the cursor.
+                if ($extent.EndOffset -eq $cursor -and $foreach.MoveNext()) {
+                    $nextToken = $foreach.Current
+                    if ($nextToken.Extent.StartOffset -eq $cursor) {
+                        $tokenToChange = $nextToken
+                    }
+                }
+                break
+            }
+        }
+
+        if ($tokenToChange -ne $null) {
+            $extent = $tokenToChange.Extent
+            $tokenText = $extent.Text
+            if ($tokenText[0] -eq '"' -and $tokenText[-1] -eq '"') {
+                # Switch to no quotes
+                $replacement = $tokenText.Substring(1, $tokenText.Length - 2)
+            } elseif ($tokenText[0] -eq "'" -and $tokenText[-1] -eq "'") {
+                # Switch to double quotes
+                $replacement = '"' + $tokenText.Substring(1, $tokenText.Length - 2) + '"'
+            } else {
+                # Add single quotes
+                $replacement = "'" + $tokenText + "'"
+            }
+
+            [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+                $extent.StartOffset,
+                $tokenText.Length,
+                $replacement
+            )
+        }
+    }
+
+# Enable tab completion for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
+# Enable tab completion for Winget
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
--- windows/README.md
+++ windows/README.md
@@ -1,6 +1,6 @@
 # Windows
 
-This folder contains all the necessary files and steps to customize a new Windows 11 machine to be able to start building apps 🚀. This is done using a combination of [Windows Package Manager](https://learn.microsoft.com/windows/package-manager/winget/) (WinGet) [Configuration File](https://learn.microsoft.com/windows/package-manager/configuration/) (*configuration.dsc.yaml*) that will work with the WinGet command line interface (`winget configure --file [path: configuration.dsc.yaml]`) and manual tasks.
+This folder contains all the necessary files and steps to customize a new Windows 11 machine with a fancy experience across all its terminal options such as Windows PowerShell, Linux Subsystem on Windows and Windows Terminal 🎨. This is done using a combination of [Windows Package Manager](https://learn.microsoft.com/windows/package-manager/winget/) (WinGet) [Configuration File](https://learn.microsoft.com/windows/package-manager/configuration/) (*configuration.dsc.yaml*) that will work with the WinGet command line interface (`winget configure --file [path: configuration.dsc.yaml]`) and manual tasks.
 
 When run, the `configuration.dsc.yaml` file will install the following list of applications:
 
@@ -14,13 +14,25 @@ The `configuration.dsc.yaml` file will also apply the following changes on the t
 * ~~Enable Virtual Machine Platform optional feature~~
 * ~~Install [Ubuntu for WSL](https://learn.microsoft.com/windows/wsl/)~~
 * Disable [User Account Control (UAC)](https://learn.microsoft.com/windows/security/application-security/application-control/user-account-control/)
-* Set up a [Dev Drive](https://learn.microsoft.com/windows/dev-drive/) of 64Gb using a Virtual Hard Disk (VHD) with a per-user directory path location to store the Dev Drive
+* Set up a [Dev Drive](https://learn.microsoft.com/windows/dev-drive/) of 64Gb using a Virtual Hard Disk (VHD) with a per-user directory path location
 * Configure packages cache location on Dev Drive
+* Reinstate the Git configurations using the [config files](../git/) stored in this repository
+* Set up Windows PowerShell
 
 > [!IMPORTANT]
 > The [PowerShell Desired State Configuration (DSC)](https://learn.microsoft.com/en-us/powershell/dsc/overview?view=dsc-2.0) that enable Windows optional features has been commented out due to an error with winget configuration calls to DSC resource. For further details, see: <https://github.com/microsoft/winget-cli/issues/4264>.
 
-## How do I use this folder? 🤔
+## Requirements
+
+Before proceeding with the `winget configure` command, ensure that your [PowerShellGet](https://learn.microsoft.com/en-us/powershell/gallery/powershellget/install-powershellget) version is `1.6.0` or higher. This is crucial for the correct machine setup. PowerShell 6+ already has a higher version of PowerShellGet built-in, however, Windows PowerShell 5.1 ships an older version of PowerShellGet which doesn't support installing prerelease modules.
+
+The WinGet configuration file provided in this repository has been designed to work with Windows PowerShell 5.1, therefore, we need to install the latest PowerShellGet (if not yet) by running the following command from an elevated Windows PowerShell session:
+
+```powershell
+Install-Module -Name PowerShellGet -Force; exit
+```
+
+## How to use this folder? 🤔
 
 To use this folder, simple follow the steps below.
 
@@ -32,9 +44,29 @@ To use this folder, simple follow the steps below.
 winget configure --file [path: configuration.dsc.yaml]
 ```
 
-### Microsoft Office 365
+![Screenshot of winget execution](../assets/winget.png)
 
-**Microsoft Office 365** requires its own subsection in this document. In earlier versions of **Microsoft Office**, users could customize the installation by selecting specific applications. In **Office 365**, this is no longer possible. The full Office suite of applications will be installed, whether it is an MSI installation from ISO or a Click-to-Run installation, and it is no longer possible to remove or add individual Office applications.
+### PowerShell Profile
+
+A [PowerShell profile](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles) is a script that a PowerShell host loads and executes automatically every time you start that host, letting you customize your environment. It's similar to `.zprofile` and `.zshrc` files in zsh. The script is, in effect, dot-sourced, so any alias, variables, functions, and the like that you define in a profile script remain available in the PowerShell session, which is incredibly handy. I use profiles to create a fancy and productive PowerShell experience.
+
+The [profile](Profile.ps1) available in this folder reflects my preferred setup. It contains modules that help me boost my experience using PowerShell such as [PSReadLine](https://github.com/PowerShell/PSReadLine), which makes PowerShell behave like zsh (that is my favorite shell in GNU/Linux) by giving you substring history search, incremental history search, and awesome tab-completion 🤯. These features significantly enhance productivity by allowing quick access to previously entered commands and providing intelligent suggestions for completing commands.
+
+To automate the setup of Windows PowerShell, the [WinGet configuration file](configuration.dsc.yaml#L145) has its own `resource` section that stores the available [PowerShell profile](Profile.ps1) under the user's home directory and install any required PowerShell module as part of the machine setup.
+
+#### PowerShell modules installed
+
+* [CompletionPredictor](https://github.com/PowerShell/CompletionPredictor)
+* [PSReadLine](https://github.com/PowerShell/PSReadLine)
+* [Posh-Git](https://github.com/dahlbyk/posh-git)
+* [Termnal-Icons](https://github.com/devblackops/Terminal-Icons)
+
+> [!IMPORTANT]
+> Be aware that complex profiles can cause a significant delay in the startup of PowerShell as it is a script that needs to be executed before the prompt first shows up. For more info about how to speed up your profile, see: <https://devblogs.microsoft.com/powershell/optimizing-your-profile/>
+
+## Microsoft Office 365
+
+**Microsoft Office 365** is worth a mention in this document. In earlier versions of **Microsoft Office**, users could customize the installation by selecting specific applications. However, in **Office 365**, this is no longer possible. The full Office suite of applications will be installed, whether it is an MSI installation from ISO or a Click-to-Run installation, and it is no longer possible to remove or add individual Office applications.
 
 To achieve this custom installation behavior, Microsoft released the [Office Deployment Tool (ODT)](https://learn.microsoft.com/microsoft-365-apps/deploy/overview-office-deployment-tool) — a command-line tool that provides more control over an Office installation. With ODT, you can define which products and languages are installed, how those products should be updated, and whether or not to display the installation experience.
 
@@ -56,9 +88,10 @@ The final result will be a clean installation of Microsoft 365 Apps with only:
 You can [customize](https://learn.microsoft.com/microsoft-365-apps/deploy/office-deployment-tool-configuration-options) this list by updating the `configuration-Office365-x64.xml` file with your required apps.
 
 >[!NOTE]
-> We're aiming to streamline our WinGet configuration by automating the Office 365 Apps installation as well as other features! Currently, this step requires manual intervention, which isn't ideal. If you have WinGet experience, we encourage you to submit a pull request adding this functionality. Help us make this happen! 🥇
+> We're aiming to streamline our WinGet configuration file by automating the Office 365 Apps installation as well as other features! Currently, this step requires manual intervention, which isn't ideal. If you have WinGet experience, we encourage you to submit a pull request adding this functionality. Help us make this happen! 🥇
 
 ## References 📚
 
 * [DSC Samples](https://github.com/microsoft/winget-dsc/tree/main/samples)
 * [Intelligent Apps Dev Box Customization](https://github.com/microsoft/devcenter-examples/tree/main/devbox-intelligent-apps)
+* [Set up a WSL development environment](https://learn.microsoft.com/windows/wsl/setup/environment)
